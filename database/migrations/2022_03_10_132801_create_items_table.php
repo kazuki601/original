bfe8743d6b74dc6d644d7c2f89fd411286fa1136
DESKTOP-4VMJ9PI\西條一樹 <riana.masato5005@gmail.com>
2022-11-12 13:32:56 +0900

    saijo221112

--- database/migrations/2022_03_10_132801_create_items_table.php
+++ database/migrations/2022_03_10_132801_create_items_table.php
@@ -16,7 +16,7 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id')->unsigned()->index();
-            $table->string('name', 100)->index();
+            $table->string('name', 100)->charset("utf8")->index();
             $table->string('status', 100)->default('active');
             $table->smallInteger('type')->nullable();
             $table->string('detail', 500)->nullable();
